e9a83e237a35b78815c4ae74ed788307e9e7b0c2
Bishara1 <31422158+Bishara10@users.noreply.github.com>
2024-08-11 11:35:28 +0300

    Update db_connection.py
    change pwd here

--- db_connection.py
+++ db_connection.py
@@ -1,6 +1,6 @@
 import pymongo
 
-url = mongodb+srv://bish150b:XWMy2SRuyRaDIAIK@cluster0.g4t9j0f.mongodb.net/
+url = 'mongodb+srv://bish150b:XWMy2SRuyRaDIAIK@cluster0.g4t9j0f.mongodb.net/'
 client = pymongo.MongoClient(url)
 
 db = client['adhd']
