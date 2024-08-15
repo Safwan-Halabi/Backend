0836424ffb73d18c9faeaf5d5bebd103463875af
Bishara1 <31422158+Bishara10@users.noreply.github.com>
2024-08-15 09:11:27 +0300

    Update db_connection.py
    fixed typo

--- db_connection.py
+++ db_connection.py
@@ -3,7 +3,7 @@ import os
 
 db_id = os.environ.get('DATABASE_USERNAME')
 db_pwd = os.environ.get('DATABASE_PWD')
-url = f'mongodb+srv://{db_id}:{db_pwd)@cluster0.g4t9j0f.mongodb.net/'
+url = f'mongodb+srv://{db_id}:{db_pwd}@cluster0.g4t9j0f.mongodb.net/'
 client = pymongo.MongoClient(url)
 
 db = client['adhd']
