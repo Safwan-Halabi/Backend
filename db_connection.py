28f09eae9baa54cfefff495b5fb922f43c3aea79
Bishara1 <31422158+Bishara10@users.noreply.github.com>
2024-08-15 09:06:34 +0300

    Update db_connection.py
    receive database info upon deploying

--- db_connection.py
+++ db_connection.py
@@ -1,6 +1,9 @@
 import pymongo
+import os
 
-url = 'mongodb+srv://bish150b:XWMy2SRuyRaDIAIK@cluster0.g4t9j0f.mongodb.net/'
+db_id = os.environ.get('DATABASE_USERNAME')
+db_pwd = os.environ.get('DATABASE_PWD')
+url = f'mongodb+srv://{db_id}:{db_pwd)@cluster0.g4t9j0f.mongodb.net/'
 client = pymongo.MongoClient(url)
 
 db = client['adhd']
